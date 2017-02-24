a0426cfbaffbe78c2d85e51935751ec2fb7a42fc
Francisco <franz_568@hotmail.com>
2017-02-24 16:14:14 -0500

    z<<
    z<

--- js/admin.js
+++ js/admin.js
@@ -6,6 +6,58 @@ $(document).ready(function(e) {
 		$("#ruc").val($.QueryString["ruc"])
 		getProgramaciones($.QueryString["ruc"]);
 	}
+	
+	
+	
+	$( ".autocomplete" ).on( "listviewbeforefilter", function ( e, data ) {        
+		var $ul = $(this);                        // $ul refers to the shell unordered list under the input box
+        var value = $( data.input ).val();        // this is value of what user entered in input box
+		var dropdownContent = "" ;                // we use this value to collect the content of the dropdown
+        $ul.html("") ;                            // clears value of set the html content of unordered list
+        
+        // on third character, trigger the drop-down
+        if ( value && value.length > 3 ) {
+			  $('.autocomplete').show();           
+			  $ul.html( "<li><div class='ui-loader'><span class='ui-icon ui-icon-loading' ></span></div></li>" );
+			  $ul.listview( "refresh" );
+			 
+			 $.ajax({
+				url : "http://www.meridian.com.pe/ServiciosMovil/AntaresAduanas/Movil/WS_Movil.asmx/BuscarCLIENTE",
+				type: "POST",
+				//crossDomain: true,
+				dataType : "json",
+				data : '{"nroRUC":"' + value + '"}',
+				//contentType: "xml",
+				contentType: "application/json; charset=utf-8",
+				success : function(data, textStatus, jqXHR) {
+				resultado = $.parseJSON(data.d);
+					 
+					if ( resultado.length > 0 ){
+						var count = 0;
+						for (var i = 0; i<resultado.length;i++){
+							dropdownContent += "<li id='" + resultado[i].cliente + "'>" + resultado[i].nombre + "</li>";
+							$ul.html( dropdownContent );
+							$ul.listview( "refresh" );
+							$ul.trigger( "updatelayout"); 							 
+						}
+					} 
+				},
+		
+				error : function(jqxhr) 
+				{
+				   //console.log(jqxhr);	
+				   alerta('Error de conexi\u00f3n, contactese con sistemas!');
+				}
+		
+			});		
+			 /* $.each(response, function( index, val ) {
+				dropdownContent += "<li>" + val + "</li>";
+				$ul.html( dropdownContent );
+				$ul.listview( "refresh" );
+				$ul.trigger( "updatelayout");  
+			  });*/
+        }
+      })
 
 	//getProgramaciones();
 	$("#actualizar").click(function(e) {
@@ -34,7 +86,13 @@ $(document).ready(function(e) {
 });
 
  
-
+ $( document).on( "click", ".autocomplete li", function() { 
+	  $("#ruc").val($(this).attr("id"));     
+      var selectedItem = $(this).html();
+	 //alert($(this).attr("id"));
+      $('.autocompletePanel .ui-filterable input').val(selectedItem);   
+      $('.autocomplete').hide();     
+    });
 
 function alertDismissed(){
 }
